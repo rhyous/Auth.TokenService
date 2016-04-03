a0e2f768fa9286d1a34dc0eb67e3152ac7d32d61
rhyous <rhyous@yahoo.com>
2016-04-03 10:52:52 -0600

    Add Interface for AuthenticationTokenService

---
+++ src/Rhyous.Auth.TokenService/Interfaces/IAuthenticationTokenService.cs
@@ -0,0 +1,15 @@
+﻿using Rhyous.Auth.TokenService.Model;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace Rhyous.Auth.TokenService.Interfaces
+{
+    [ServiceContract]
+
+    public interface IAuthenticationTokenService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+        string Authenticate(Credentials creds);
+    }
+}
--- src/Rhyous.Auth.TokenService/Services/AuthenticationTokenService.svc.cs
+++ src/Rhyous.Auth.TokenService/Services/AuthenticationTokenService.svc.cs
@@ -3,16 +3,14 @@ using System.ServiceModel.Activation;
 using System.ServiceModel.Web;
 using Rhyous.Auth.TokenService.Business;
 using Rhyous.Auth.TokenService.Database;
+using Rhyous.Auth.TokenService.Interfaces;
 using Rhyous.Auth.TokenService.Model;
 
 namespace Rhyous.Auth.TokenService.Services
 {
-    [ServiceContract]
     [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
-    public class AuthenticationTokenService
+    public class AuthenticationTokenService : IAuthenticationTokenService
     {
-        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
-        [OperationContract]
         public string Authenticate(Credentials creds)
         {
             if (creds == null && WebOperationContext.Current != null)
